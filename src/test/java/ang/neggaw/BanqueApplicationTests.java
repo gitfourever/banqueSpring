4e8708f9690fdcd7cac955402ae9a82a169f4e28
gitfourever <cvofang@gmail.com>
2020-09-08 09:57:50 +0200

    Creation du projet banque

---
+++ src/test/java/ang/neggaw/BanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package ang.neggaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
