c21342a9a223e29e63cf39a1084e82436a29e46f
gitfourever <cvofang@gmail.com>
2020-09-08 12:53:45 +0200

    creation package DAO en utilisant Spring_Api_Rest

--- src/main/java/ang/neggaw/BanqueApplication.java
+++ src/main/java/ang/neggaw/BanqueApplication.java
@@ -3,11 +3,13 @@ package ang.neggaw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.xml.ws.Endpoint;
+
 @SpringBootApplication
 public class BanqueApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BanqueApplication.class, args);
     }
-
 }
---
+++ src/main/java/ang/neggaw/dao/CompteRepository.java
@@ -0,0 +1,9 @@
+package ang.neggaw.dao;
+
+import ang.neggaw.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CompteRepository extends JpaRepository<Compte, String> {
+}
